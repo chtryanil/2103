51197911842473e4bb3974a6ec5beacbfea7675c
chtryanil <chtryanil@outlook.com>
2024-10-24 02:14:46 +1100

    Removed hardcoded API keys and updated to use environment variables

--- search/search.py
+++ search/search.py
@@ -1,8 +1,9 @@
 import requests
+import os
 
 def search_google(query, num_results=5):
-    api_key = "AIzaSyDfvGm4JaDMoXqi2F0hBYkwUj2o10BAnEM"
-    search_engine_id = "c4217e3174fdf468b"  # You need to create this in Google Programmable Search Engine
+    api_key = os.getenv("GOOGLE_API_KEY")
+    search_engine_id = os.getenv("SEARCH_ENGINE_ID")
     url = f"https://www.googleapis.com/customsearch/v1?key={api_key}&cx={search_engine_id}&q={query}&num={num_results}"
 
 
